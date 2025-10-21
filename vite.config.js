1f448a6b235e8facfbeb7093fae367c6b9432e7d
vinimathew22 <vinimathew2210@gmail.com>
2025-10-21 20:39:43 +0530

    completed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-todo/"
+  base: "https://vinimathew22.github.io/react-todo/"
 })
